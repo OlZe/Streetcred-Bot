4d0f01be14a1fd192852781f437b963d13238b38
OlZe <oliverzeidler97@gmail.com>
2020-06-15 22:23:12 +0200

    bot now answers telegram messages with "hello world!"

--- streetcred_bot.php
+++ streetcred_bot.php
@@ -5,4 +5,21 @@ define("API_URL", "https://api.telegram.org/bot".API_AUTH_TOKEN."/");
 
 
 
+function replyToMessage($chat_id, $message_id, $replyText) {
+    $parameters = array(
+        "chat_id" => $chat_id,
+        "reply_to_message_id" => $message_id,
+        "text" => $replyText,
+        "method" => "sendMessage");
+    header("Content-Type: application/json");
+    echo json_encode($parameters);
+}
+
+
+
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+$message = $update["message"];
+replyToMessage($message["chat"]["id"], $message["message_id"], "hello world!")
+
 ?>
