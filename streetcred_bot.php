4d12cbf8b59010fb5af29ad6ba3519ee812e6425
OlZe <oliverzeidler97@gmail.com>
2020-06-18 20:30:55 +0200

    Moved all code into either Dao, Service or Controller class

--- streetcred_bot.php
+++ streetcred_bot.php
@@ -3,55 +3,80 @@
 define("API_AUTH_TOKEN", file_get_contents("./auth_token"));
 define("API_URL", "https://api.telegram.org/bot".API_AUTH_TOKEN."/");
 define("GIVE_CRED_COMMAND", "respect");
-handleWebRequest();
+(new Controller())->handleWebRequest();
 
+class Controller {
+    private $service;
 
-function handleWebRequest()  {
-    $request = json_decode(file_get_contents("php://input"), true);
-    if(isTextMessage($request)) {
-        handleTextMessage($request["message"]);
+    public function __construct() {
+        $this->$service = new Service();
     }
-    else {
-        echo("unsupported request");
+
+    public function handleWebRequest()  {
+        $request = json_decode(file_get_contents("php://input"), true);
+        $answer = null;
+        if($this->isTextMessage($request)) {
+            $answer = $this->$service->handleTextMessage($request["message"]);
+        }
+        else {
+            $answer = "unsupported request";
+        }
+        $this->respondWebRequest($answer);
+    }
+    
+    private function respondWebRequest($body) {
+        header("Content-Type: application/json");
+        echo(json_encode($body));
+    }
+    
+    private function isTextMessage($request) {
+        return isset($request["message"]) && isset($request["message"]["text"]);
     }
 }
+    
+class Service {
+    private $dao;
+    
+    public function __construct() {
+        $this->$dao = new Dao();
+    }
 
-function handleTextMessage($message) {
-    if(strpos($message["text"], GIVE_CRED_COMMAND) === 0) {
-        if(messageIsReply($message)) {
+    public function handleTextMessage($message) {
+        $answer = null;
+        if(strpos($message["text"], GIVE_CRED_COMMAND) === 0) {
+            $replyText = $this->handleGiveCredCommand($message);
+            if($replyText != null) {
+                $answer = $this->prepareReplyToMessage($message, $replyText);
+            }
+        }
+        return $answer;
+    }
+        
+    private function handleGiveCredCommand($message) {
+        $text = null;
+        if($this->messageIsReply($message)) {
             $replyRecieverName = $message["reply_to_message"]["from"]["first_name"];
             $replyRecieverId = $message["reply_to_message"]["from"]["id"];
-            $credDao = new StreetcredDao;
-            $replyRecieverCred = $credDao->addCredToUser($message["chat"]["id"], $replyRecieverId, 1);
-            replyToMessage($message["chat"]["id"], $message["message_id"], "Streetcred: ".$replyRecieverCred);
+            $replyRecieverCred = $this->$dao->addCredToUser($message["chat"]["id"], $replyRecieverId, 1);
+            $text = "Streetcred: ".$replyRecieverCred;
         }
+        return $text;
+    }
+
+    private function messageIsReply($message) {
+        return isset($message["reply_to_message"]);
+    }
+
+    private function prepareReplyToMessage($message, $text) {
+        return array(
+            "chat_id" => $message["chat"]["id"],
+            "reply_to_message_id" => $message["message_id"],
+            "text" => $text,
+            "method" => "sendMessage");
     }
 }
 
-function replyToMessage($chat_id, $message_id, $replyText) {
-    $parameters = array(
-        "chat_id" => $chat_id,
-        "reply_to_message_id" => $message_id,
-        "text" => $replyText,
-        "method" => "sendMessage");
-    respondWebRequest($parameters);
-}
-
-function isTextMessage($request) {
-    return isset($request["message"]) && isset($request["message"]["text"]);
-}
-
-function messageIsReply($message) {
-    return isset($message["reply_to_message"]);
-}
-
-function respondWebRequest($body) {
-    header("Content-Type: application/json");
-    echo(json_encode($body));
-}
-
-
-class StreetcredDao {
+class Dao {
     public function getCredForUser($chat_id, $user_id) {
         $cred = 0;
         $file = $this->getCredForChat($chat_id);
@@ -91,6 +116,8 @@ class StreetcredDao {
         file_put_contents("./".$chat_id, json_encode($credData));
     }
 }
+
+
 
 
 ?>
